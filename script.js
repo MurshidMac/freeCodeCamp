98663e4d125ed3334ae77e9eac1aaacff34e7227
MurshidMac <mh.murshidco.uk@outlook.com>
2017-04-29 13:02:08 +0530

    Added the Remove Class jQuery

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Custom JQuery Functions 
+// Animation Adding a Class 
 <script>
   $(document).ready(function() {
 	// accessing the button element from jquery	
 	$("button").addClass("animated shake")	// this is a custom animation from css class
 	$("#target").addClass("animated fadeOut")	// this s custom animation funtion to fadeout an ID from a div or anyother tags	
+  });
+</script>
+
+// Animation Removing a Class
+<script>
+  $(document).ready(function() {
+    $("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $("#target3").addClass("animated fadeOut");
+    $("button").removeClass("btn-default")	// this is a special class to remove files from the elements
+    
+    
   });
 </script>
 
