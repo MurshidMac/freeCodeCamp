d3a1544d1532621b441e56bc4182a8a02208b4e0
MurshidMac <mh.murshidco.uk@outlook.com>
2017-04-29 11:42:33 +0530

    Fundemental jQuery Functions

---
+++ script.js
@@ -0,0 +1,9 @@
+// Custom JQuery Functions 
+<script>
+  $(document).ready(function() {
+	// accessing the button element from jquery	
+	$("button").addClass("animated shake")	// this is a custom animation from css class
+	$("#target").addClass("animated fadeOut")	// this s custom animation funtion to fadeout an ID from a div or anyother tags	
+  });
+</script>
+
